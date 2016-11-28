a64cc60f94c0bacc92b9021e01fd8ccd9418c7c0
Brett Sanders <bsand08@gmail.com>
2016-11-27 20:19:55 -0800

    init js and comments

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// get the height and width of the page
+// define array of colors to use to create painting
+// for each pixel in the height of the page
+//    get a random color (from the array)
+//    paint a bar horizontally in that color

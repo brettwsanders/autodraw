803f2a786ef729c63d9c3bc3b3d94981806bdf22
Brett Sanders <bsand08@gmail.com>
2016-11-27 20:51:05 -0800

    make single blue bar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
+console.log('i am initialized');
+
+
 // get the height and width of the page
+var body = document.body;
+var html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+    html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+console.log('the height is', height);
+
+
 // define array of colors to use to create painting
+var colors = ['blue', 'yellow', 'red'];
+
+
 // for each pixel in the height of the page
 //    get a random color (from the array)
 //    paint a bar horizontally in that color
+
+var lineEl = document.createElement("DIV");
+lineEl.setAttribute("style", "height: 1px; background-color: blue;");
+body.appendChild(lineEl);
+
